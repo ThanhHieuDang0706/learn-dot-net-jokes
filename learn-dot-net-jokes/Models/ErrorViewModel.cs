7b8fe471abb9d6a6a7433bdcced5605509f26802
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2022-12-26 15:25:38 +0700

    Add project files.

---
+++ learn-dot-net-jokes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace learn_dot_net_jokes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
